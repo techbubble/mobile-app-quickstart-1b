cbc10b792c71851a93af4ccdd99e1743e9e31984
Nik Kalyani <nik@kalyani.com>
2014-10-14 04:47:24 +0000

    Changes made by Nik Kalyani (nik@kalyani.com).

---
+++ Mobile App Quickstart 1b/js/app.js
@@ -0,0 +1,39 @@
+var watch = 0;
+var context;
+var target;
+var xPos;
+var yPos;
+var canvas;
+
+var onDeviceReady = function() {
+    navigator.splashscreen.hide(); 
+    
+    /* Get screen size */
+    var iW = window.innerWidth;
+    var iH = window.innerHeight;
+
+    /* Canvas code goes below this line */
+
+    
+    /* Image code goes below this line */
+
+    
+    /* Accelerometer code goes below this line */
+
+    
+};
+
+document.addEventListener('deviceready', onDeviceReady);
+
+
+function success(accel)
+{
+    /* Success code goes here */
+
+
+}
+
+function failure()
+{
+    alert("Error");
+}
